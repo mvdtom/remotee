367624ab3e0adde658f50cef6e20d7c428ef5ef5
Mikhail Derevyashkin <mvdtom@gmail.com>
2020-03-05 02:12:36 +0700

    additional test

--- src/cmd/generator_test.go
+++ src/cmd/generator_test.go
@@ -11,24 +11,23 @@ const offset = 40
 const templateValue = 0xFF
 const insertValue = 0xAA
 
-func TestFullFilling(t *testing.T) {
-	gen := createGet()
+func TestSimpleFullFilling(t *testing.T) {
+	gen := createGen()
 	result := makeFilledSlice(templateLen, 0x00)
 	read, err := gen.Read(result)
 
-	if err != io.EOF {
-		t.Error("Result error is incorrect:", err)
-	}
-	if read != templateLen {
-		t.Errorf("Invalid read value. Expected: [%d]. Actual: [%d]", templateLen, read)
-	}
+	checkErr(io.EOF, err, t)
+	checkRead(templateLen, read, t)
+	checkValues(result, 0, t)
+}
 
+func checkValues(result []byte, read int, t *testing.T) {
 	for i, v := range result {
 		var expected byte
 		switch {
-		case i < offset:
+		case i+read < offset:
 			expected = templateValue
-		case i >= offset+insertLen:
+		case i+read >= offset+insertLen:
 			expected = templateValue
 		default:
 			expected = insertValue
@@ -39,7 +38,35 @@ func TestFullFilling(t *testing.T) {
 	}
 }
 
-func createGet() Generator {
+func checkRead(expected, actual int, t *testing.T) {
+	if actual != expected {
+		t.Errorf("Invalid read value. Expected: [%d]. Actual: [%d]", expected, actual)
+	}
+}
+
+func checkErr(expected, actual error, t *testing.T) {
+	if actual != expected {
+		t.Errorf("Error is incorrect. Expected: [%v]. Actual: [%v]", expected, actual)
+	}
+}
+
+func TestFillingByParts(t *testing.T) {
+	gen := createGen()
+	result := makeFilledSlice(50, 0x0)
+	read, err := gen.Read(result)
+
+	checkErr(nil, err, t)
+	checkRead(50, read, t)
+	checkValues(result, 0, t)
+
+	read, err = gen.Read(result)
+
+	checkErr(io.EOF, err, t)
+	checkRead(50, read, t)
+	checkValues(result, 50, t)
+}
+
+func createGen() Generator {
 	template := makeFilledSlice(templateLen, templateValue)
 	insert := makeFilledSlice(insertLen, insertValue)
 	gen := Generator{template: &template, insertOffset: offset, insert: insert}
